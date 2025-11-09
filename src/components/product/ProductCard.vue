75fadc86f4891cd2554b197222119b2f1462d5ac
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-09 11:06:29 -0300

    feat: adds ProductCard.vue to src/components/product/

---
+++ src/components/product/ProductCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <Card class="w-full">
+    <template #header>
+      <img alt="user header" src="https://primefaces.org/cdn/primevue/images/usercard.png" />
+    </template>
+    <template #title>Advanced Card</template>
+    <template #subtitle>Card subtitle</template>
+    <template #content>
+      <p class="m-0">
+        Lorem ipsum dolor sit amet, consectetur adipis elit. Inventore sed consequuntur error
+        periores earum nam nobis, culpa!
+      </p>
+    </template>
+    <template #footer>
+      <div class="flex gap-4 mt-1">
+        <Button label="Cancel" severity="secondary" variant="outlined" class="w-full" />
+        <Button label="Save" class="w-full" />
+      </div>
+    </template>
+  </Card>
+</template>
+
+<script setup lang="ts">
+import Card from 'primevue/card'
+import Button from 'primevue/button'
+</script>
