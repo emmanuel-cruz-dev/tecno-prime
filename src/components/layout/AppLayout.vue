49ae0bd219ed50b1788d1f9633328faf0c8bcd47
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-11-17 16:51:18 -0300

    feat: updates styles in src/components/layout/AppLayout.vue

--- src/components/layout/AppLayout.vue
+++ src/components/layout/AppLayout.vue
@@ -2,7 +2,7 @@
   <div class="min-h-screen flex flex-col">
     <AppNavbar />
     <main class="grow">
-      <div class="w-full p-4 md:p-6">
+      <div class="w-full p-4 md:p-6 lg:py-10 lg:px-20">
         <RouterView />
       </div>
     </main>
